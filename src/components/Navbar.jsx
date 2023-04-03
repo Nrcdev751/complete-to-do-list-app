9d00a8bb0deeca8b739905d71ee3a6d308072c5f
Narongchai (lmDOW) <91950802+lmDOW01@users.noreply.github.com>
2023-04-04 05:54:14 +0700

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 
+// logo
+import navlogo from '../assets/logo.png'
+
 const Navbar = () => {
   return (
     <section className="navbar" id="nav">
     <nav>
      <div className="nav-container">
      <ul className="nav-logo">
-                <img className="" src="src/assets/logo.png" alt="to-do-logo" />
+                <img className="" src={navlogo} alt="to-do-logo" />
             </ul>
             <ul className="nav-menu">
                 <li>หน้าแรก</li>
